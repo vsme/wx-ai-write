e67d42bca20301212434448a90762951626c8936
yaavi <27679365@qq.com>
2023-03-09 00:42:55 +0800

    init

---
+++ src/pages/index/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <view class="index">
+    <Counter />
+    <button>
+      button
+    </button>
+  </view>
+</template>
+
+<script setup>
+import './index.scss'
+import Counter from '../../components/Counter.vue'
+</script>
